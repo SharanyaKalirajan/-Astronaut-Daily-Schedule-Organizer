31be9e9f871fa16f425e1eb22989280ea2aed58d
SharanyaKalirajan <164001667+SharanyaKalirajan@users.noreply.github.com>
2024-07-28 18:19:20 +0530

    Create TaskNotifier.java

---
+++ TaskNotifier.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskNotifier {
+    private List<Observer> observers;
+
+    public TaskNotifier() {
+        observers = new ArrayList<>();
+    }
+
+    public void subscribe(Observer observer) {
+        observers.add(observer);
+    }
+
+    public void unsubscribe(Observer observer) {
+        observers.remove(observer);
+    }
+
+    public void notifyObservers(String message) {
+        for (Observer observer : observers) {
+            observer.update(message);
+        }
+    }
+}
